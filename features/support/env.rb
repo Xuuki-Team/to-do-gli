8c7d57812fd79277752ad43c415fcd318d01d291
Joel Nash <joelnash@xuuki.xyz>
2025-06-06 22:22:50 +0100

    create fake_home if not exist added

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,6 +2,7 @@ Before do
   @real_home = ENV['HOME']
   fake_home = File.join('/tmp','fake_home')
   FileUtils.rm_rf fake_home, :secure => true
+  FileUtils.mkdir_p fake_home
   ENV['HOME'] = fake_home
 end
 After do
